488c28ff36830252265c8b754ea5d5ac99b5a6d4
leona rose <lrose@consoneo.com>
2024-08-05 18:10:21 +0200

    go

--- brawlstats_app.py
+++ brawlstats_app.py
@@ -1,1 +1,18 @@
 import streamlit as st 
+import requests
+
+st.title('BrawlStats')
+st.write('A Brawl Stars Stats App')
+
+params = st.text_input('Your Tag')
+
+API_KEY = st.secrets(['api'])
+
+def get_request(api_key, params):
+    headers = 'Authentication : Bearer {API_KEY}'
+    st.write('headers')
+    url = 'https://api.brawlstars.com/v1/players/'
+    response = requests.get(url, params=params, headers)
+    return response
+
+st.title('Brawl Stats')
