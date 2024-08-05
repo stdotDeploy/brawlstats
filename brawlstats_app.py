88ecfa1a381061ebf54c00d26bcef4120171b4be
leona rose <lrose@consoneo.com>
2024-08-05 18:12:03 +0200

    go

--- brawlstats_app.py
+++ brawlstats_app.py
@@ -8,11 +8,10 @@ params = st.text_input('Your Tag')
 
 API_KEY = st.secrets(['api'])
 
+st.title('Brawl Stats')
 def get_request(api_key, params):
     headers = 'Authentication : Bearer {API_KEY}'
     st.write('headers')
     url = 'https://api.brawlstars.com/v1/players/'
     response = requests.get(url, params=params, headers)
     return response
-
-st.title('Brawl Stats')
