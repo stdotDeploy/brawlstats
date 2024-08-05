e80e4db8ceaba73abaef826eb7485ca4de0509e3
leona rose <lrose@consoneo.com>
2024-08-05 18:35:50 +0200

    check api connection

--- brawlstats_app.py
+++ brawlstats_app.py
@@ -4,7 +4,7 @@ import requests
 def get_request():
     API_KEY = st.secrets['api']['API_KEY']
     headers = {'Authorization': f'Bearer {API_KEY}'}
-    st.write('headers')
+    st.write(headers)
     url = 'https://api.brawlstars.com/v1/players/'
     response = requests.get(url, params=params, headers=headers)
     return response
